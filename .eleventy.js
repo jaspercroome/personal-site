168fe0fb7787306dfabbed9f5a6ddf9df24a7b27
Jasper <jaspercroome@protonmail.com>
2026-07-25 06:56:56 -0700

    chore: add rss

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPassthroughCopy("src/images");
   return {
     dir: { input: "src", output: "_site" },
